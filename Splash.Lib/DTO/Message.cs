a324189f065b9bfb9870135382d49107d26be98b
aswin vp <aswinprabhakaran@gmail.com>
2019-12-08 20:51:45 +0530

    before adding files into resource

---
+++ Splash.Lib/DTO/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Splash.Lib.DTO
+{
+    [DataContract]
+    public class Message
+    {
+        [DataMember]
+        public string Header { get; set; }
+        [DataMember]
+        public string Body { get; set; }
+        [DataMember]
+        public DateTime Time { get; set; }
+
+        public Message(string header, DateTime time)
+        {
+            Header = header;
+            Time = time;
+        }
+    }
+}
---
+++ Splash.Server/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace Splash.Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void StartServer()
+        {
+             
+        }
+    }
+}
